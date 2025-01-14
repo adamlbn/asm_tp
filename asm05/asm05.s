855c4a619585f080924c18cd2c814a270dfc2012
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-14 15:00:11 +0100

    Update asm05.s

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -6,40 +6,38 @@ section .text
     global _start
 
 _start:
-    mov rdi, [rsp + 8]
-    cmp rdi, 1
+    mov rdi, [rsp]       
+    cmp rdi, 1           
     jle no_input_error
 
-    mov rsi, [rsp + 16]
-    mov rdi, 1
+    mov rsi, [rsp + 8]   
     call print_string
 
-    mov rax, 60
-    xor rdi, rdi
+    mov rax, 60          
+    xor rdi, rdi         
     syscall
 
 no_input_error:
-    mov rdi, 1
+    mov rdi, 1           
     mov rsi, error_msg
     mov rdx, error_msg_len
-    mov rax, 1
+    mov rax, 1           
     syscall
 
-    mov rax, 60
-    mov rdi, 1
+    mov rax, 60          
+    mov rdi, 1           
     syscall
 
 print_string:
     xor rdx, rdx
-    mov rcx, rsi
-.next_char:
-    cmp byte [rcx], 0
-    je .done
+.find_length:
+    cmp byte [rsi + rdx], 0
+    je .write_string
     inc rdx
-    inc rcx
-    jmp .next_char
-.done:
-    mov rax, 1
-    mov rdi, 1
+    jmp .find_length
+
+.write_string:
+    mov rax, 1          
+    mov rdi, 1           
     syscall
     ret
