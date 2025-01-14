77af0a5502f601ac568ac6cfaf4b61dbd06fa3f0
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-14 16:50:32 +0100

    Update asm07.s

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -1,116 +1,110 @@
 section .data
-    result_msg db "", 0
-    result_msg_len equ $ - result_msg
-    buffer db 0
-    error_msg db "Error: No input provided", 0
+    error_msg db "Error: Invalid input", 0xA
     error_msg_len equ $ - error_msg
 
 section .bss
     num resq 1
-    result resq 1
-    temp resb 21  
+    buffer resb 32
 
 section .text
     global _start
 
 _start:
-    cmp qword [rsp + 16], 0
-    je .no_input_error
-
-    mov rsi, [rsp + 16]  
-    mov rdi, num
-    call string_to_int
-
     mov rax, 0
-    mov rcx, [num]
-    dec rcx
-    .sum_loop:
-        test rcx, rcx
-        js .sum_done
-        add rax, rcx
-        dec rcx
-        jmp .sum_loop
-    .sum_done:
-    mov [result], rax
-    
-    mov rdi, 1         
-    mov rsi, result_msg
-    mov rdx, result_msg_len
-    mov rax, 1          
+    mov rdi, 0
+    mov rsi, buffer
+    mov rdx, 32
     syscall
 
-    mov rax, [result]
-    mov rdi, temp        
-    call int_to_string
-    mov rsi, rdi        
-    call print_string
+    mov rsi, buffer
+    call string_to_int
+    cmp rax, -1
+    je .invalid_input
+    mov [num], rax
 
-    mov rax, 60          
-    xor rdi, rdi        
-    syscall
-
-.no_input_error:
-    mov rdi, 1
-    mov rsi, error_msg
-    mov rdx, error_msg_len
-    mov rax, 1
-    syscall
+    call is_prime
+    test rax, rax
+    jz .prime
 
     mov rax, 60
     mov rdi, 1
+    syscall
+
+.prime:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+.invalid_input:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, error_msg
+    mov rdx, error_msg_len
+    syscall
+
+    mov rax, 60
+    mov rdi, 2
     syscall
 
 
 string_to_int:
     xor rax, rax
     xor rcx, rcx
+    xor rbx, rbx
+    movzx rdx, byte [rsi]
+    cmp rdx, '-'
+    jne .loop
+    inc rsi
+    mov rbx, 1
 .loop:
     movzx rdx, byte [rsi + rcx]
-    cmp rdx, 0
+    cmp rdx, 0xA
     je .end
+    cmp rdx, '0'
+    jb .invalid
+    cmp rdx, '9'
+    ja .invalid
     sub rdx, '0'
     imul rax, rax, 10
     add rax, rdx
     inc rcx
     jmp .loop
 .end:
-    mov [rdi], rax
+    test rbx, rbx
+    jz .positive
+    neg rax
+.positive:
+    ret
+.invalid:
+    mov rax, -1
     ret
 
 
-int_to_string:
-    mov rcx, 10
-    mov rbx, 0           
-    add rdi, 20         
-    mov byte [rdi], 0    
-    dec rdi
-.convert_loop:
+is_prime:
+    mov rax, [num]
+    cmp rax, 1
+    jle .not_prime
+    cmp rax, 2
+    je .prime
+    test rax, 1
+    jz .not_prime
+
+    mov rcx, 3
+.check_loop:
+    mov rdx, rcx
+    imul rdx, rdx
+    cmp rdx, rax
+    ja .prime
     xor rdx, rdx
     div rcx
-    add dl, '0'
-    mov [rdi], dl
-    dec rdi
-    test rax, rax
-    jnz .convert_loop
-    test rbx, rbx
-    jz .no_sign
-    mov byte [rdi], '-'
-    dec rdi
-.no_sign:
-    inc rdi
+    test rdx, rdx
+    jz .not_prime
+    add rcx, 2
+    jmp .check_loop
+
+.prime:
+    xor rax, rax
     ret
-
-
-print_string:
-    mov rdx, 0
-    .find_end:
-        cmp byte [rsi + rdx], 0
-        je .found_end
-        inc rdx
-        jmp .find_end
-    .found_end:
+.not_prime:
     mov rax, 1
-    mov rdi, 1        
-    syscall
     ret
-
