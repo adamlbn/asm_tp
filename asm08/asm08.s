16871e2527ac23ff5a2be424f92ae25bc0a47457
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-14 17:01:21 +0100

    Update asm08.s

--- asm08/asm08.s
+++ asm08/asm08.s
@@ -1,96 +1,115 @@
-global _start
+section .data
+    result_msg db "", 0
+    result_msg_len equ $ - result_msg
+    buffer db 0
+    error_msg db "Error: No input provided", 0
+    error_msg_len equ $ - error_msg
 
 section .bss
-    nb resb 32
-    string resb 32
+    num resq 1
+    result resq 1
+    temp resb 21
 
 section .text
+    global _start
+
 _start:
-    mov r13, [rsp]
-    cmp r13, 0x2
-    jne error
+    cmp qword [rsp + 16], 0
+    je .no_input_error
 
-    mov rsi, rsp
-    add rsi, 16
-    mov rsi, [rsi]
-    mov rdi, nb
-    mov rcx, 4
-    rep movsb
+    mov rsi, [rsp + 16]
+    mov rdi, num
+    call string_to_int
 
+    mov rax, 0
+    mov rcx, [num]
+    dec rcx
+    .sum_loop:
+        test rcx, rcx
+        js .sum_done
+        add rax, rcx
+        dec rcx
+        jmp .sum_loop
+    .sum_done:
+    mov [result], rax
+
+    mov rdi, 1
+    mov rsi, result_msg
+    mov rdx, result_msg_len
+    mov rax, 1
+    syscall
+
+    mov rax, [result]
+    mov rdi, temp
+    call int_to_string
+    mov rsi, rdi
+    call print_string
+
+    mov rax, 60
     xor rdi, rdi
-    mov r8, 0
+    syscall
 
-convertToHexa:
-    mov al, [nb + rdi]
-    cmp al, 0
-    je finishConvert
+.no_input_error:
+    mov rdi, 1
+    mov rsi, error_msg
+    mov rdx, error_msg_len
+    mov rax, 1
+    syscall
 
-    cmp rax, '0'
-    jl error
+    mov rax, 60
+    mov rdi, 1
+    syscall
 
-    cmp rax, '9'
-    jg error
 
-    sub rax, 48
-    imul r8, 10
-    add r8, rax
+string_to_int:
+    xor rax, rax
+    xor rcx, rcx
+.loop:
+    movzx rdx, byte [rsi + rcx]
+    cmp rdx, 0
+    je .end
+    sub rdx, '0'
+    imul rax, rax, 10
+    add rax, rdx
+    inc rcx
+    jmp .loop
+.end:
+    mov [rdi], rax
+    ret
 
-    inc rdi
-    jmp convertToHexa
 
-finishConvert:
-    mov rax, r8
-    mov rcx, 16
-    xor rdi, rdi
-    mov rdi, string
-
-loop:
+int_to_string:
+    mov rcx, 10
+    mov rbx, 0
+    add rdi, 20
+    mov byte [rdi], 0
+    dec rdi
+.convert_loop:
     xor rdx, rdx
     div rcx
-    push rdx
-    inc r10
-    cmp rax, 0
-    je finish
+    add dl, '0'
+    mov [rdi], dl
+    dec rdi
+    test rax, rax
+    jnz .convert_loop
+    test rbx, rbx
+    jz .no_sign
+    mov byte [rdi], '-'
+    dec rdi
+.no_sign:
+    inc rdi
+    ret
 
-    jmp loop
 
-finish:
-    xor rdi, rdi
-    mov rdi, string
-
-addString:
-    pop r11
-    cmp r11, 10
-    jb ._decimal
-    jae ._ascii
-
-    ._decimal:
-        add r11, '0'
-        jmp ._store
-    ._ascii:
-        add r11, 87
-        jmp ._store
-    ._store:
-      mov [rdi], r11
-      inc rdi
-      dec r10
-      cmp r10, 0
-      je _end
-      jmp addString
-
-_end:
-    mov byte [rdi], 10
-
-    mov rsi, string
+print_string:
+    mov rdx, 0
+    .find_end:
+        cmp byte [rsi + rdx], 0
+        je .found_end
+        inc rdx
+        jmp .find_end
+    .found_end:
     mov rax, 1
     mov rdi, 1
     syscall
-
-    mov rax, 60
-    mov rdi, 0
-    syscall
-
-error:
-    mov rax, 60
-    mov rdi, 1
-    syscall
+    ret
