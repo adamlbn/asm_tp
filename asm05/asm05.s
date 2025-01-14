2f207471acee14c631496e7607045906543d25d4
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-14 15:18:09 +0100

    Update asm05.s

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -2,42 +2,44 @@ section .data
     error_msg db "Error: No input provided", 0xA
     error_msg_len equ $ - error_msg
 
+section .bss
+    buffer resb 256
+
 section .text
     global _start
 
 _start:
-    mov rdi, [rsp]       
-    cmp rdi, 1           
+    mov rdi, [rsp + 8]
+    cmp rdi, 1
     jle no_input_error
 
-    mov rsi, [rsp + 8]   
+    mov rsi, [rsp + 16]
+    mov rdi, 1
     call print_string
 
-    mov rax, 60          
-    xor rdi, rdi         
+    mov rax, 60
+    xor rdi, rdi
     syscall
 
 no_input_error:
-    mov rdi, 1           
+    mov rdi, 1
     mov rsi, error_msg
     mov rdx, error_msg_len
-    mov rax, 1           
+    mov rax, 1
     syscall
 
-    mov rax, 60          
-    mov rdi, 1           
+    mov rax, 60
+    mov rdi, 1
     syscall
 
 print_string:
     xor rdx, rdx
-.find_length:
+.find_end:
     cmp byte [rsi + rdx], 0
-    je .write_string
+    je .found_end
     inc rdx
-    jmp .find_length
-
-.write_string:
-    mov rax, 1          
-    mov rdi, 1           
+    jmp .find_end
+.found_end:
+    mov rax, 1
     syscall
     ret
