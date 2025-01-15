b18b6134bd8d4cc450e592c35c6ef4f7ece7acb7
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-15 14:43:52 +0100

    Update asm10.s

--- asm10/asm10.s
+++ asm10/asm10.s
@@ -6,115 +6,59 @@ section .data
     newline db 10
 
 section .bss
-    result resb 32
+    result resb 32  ; Buffer pour stocker le résultat en chaîne de caractères
 
 section .text
     global _start
 
 _start:
-    mov r13, [rsp]
-    cmp r13, 4
+    ; Vérifier le nombre d'arguments
+    mov r13, [rsp]        ; r13 = argc
+    cmp r13, 4            ; 4 arguments (./asm10 + 3 nombres)
     jne _usage_error
 
-    mov rsi, rsp
-    add rsi, 16
+    ; Extraire les arguments
+    mov rsi, [rsp + 16]   ; rsi = argv[1] (premier nombre)
     call _atoi
-    mov r8, rax
+    mov r8, rax           ; r8 = premier nombre
 
-    mov rsi, rsp
-    add rsi, 24
+    mov rsi, [rsp + 24]   ; rsi = argv[2] (deuxième nombre)
     call _atoi
-    mov r9, rax
+    mov r9, rax           ; r9 = deuxième nombre
 
-    mov rsi, rsp
-    add rsi, 32
+    mov rsi, [rsp + 32]   ; rsi = argv[3] (troisième nombre)
     call _atoi
-    mov r10, rax
+    mov r10, rax          ; r10 = troisième nombre
 
-    mov rax, r8
+    ; Trouver le plus grand nombre
+    mov rax, r8           ; rax = premier nombre
     cmp rax, r9
     jge ._compare_third
-    mov rax, r9
+    mov rax, r9           ; rax = deuxième nombre
 
 ._compare_third:
     cmp rax, r10
     jge ._print_result
-    mov rax, r10
+    mov rax, r10          ; rax = troisième nombre
 
 ._print_result:
-    lea rdi, [result + 31]
-    mov byte [rdi], 0
+    ; Convertir le résultat en chaîne de caractères
+    lea rdi, [result + 31] ; Pointeur vers la fin du buffer
+    mov byte [rdi], 0      ; Null-terminate la chaîne
     dec rdi
 
-    mov rcx, 10
+    mov rcx, 10            ; Base 10 pour la conversion
 
 convert_loop:
-    xor rdx, rdx
-    div rcx
-    add dl, '0'
-    mov [rdi], dl
+    xor rdx, rdx           ; Clear rdx pour la division
+    div rcx                ; Diviser rax par 10
+    add dl, '0'            ; Convertir le reste en ASCII
+    mov [rdi], dl          ; Stocker le caractère
     dec rdi
-    cmp rax, 0
+    cmp rax, 0             ; Si le quotient est 0, on a fini
     jne convert_loop
 
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rdi + 1]
-    lea rdx, [result + 32]
-    sub rdx, rsi
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, newline
-    mov rdx, 1
-    syscall
-
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
-_usage_error:
-    mov rax, 1
-    mov rdi, 2
-    lea rsi, [usage_msg]
-    mov rdx, usage_msg.lenUsage
-    syscall
-
-    mov rax, 60
-    mov rdi, 1
-    syscall
-
-_atoi:
-    xor rax, rax
-    xor rcx, rcx
-
-.atoi_loop:
-    movzx rbx, byte [rsi + rcx]
-    test rbx, rbx
-    je .atoi_done
-
-    cmp rbx, '0'
-    jl _invalid_input
-    cmp rbx, '9'
-    jg _invalid_input
-
-    sub rbx, '0'
-    imul rax, 10
-    add rax, rbx
-    inc rcx
-    jmp .atoi_loop
-
-.atoi_done:
-    ret
-
-_invalid_input:
-    mov rax, 1
-    mov rdi, 2
-    lea rsi, [invalid_input_msg]
-    mov rdx, invalid_input_msg.lenInvalidInput
-    syscall
-
-    mov rax, 60
-    mov rdi, 1
-    syscall
+    ; Afficher le résultat
+    mov rax, 1             ; sys_write
+    mov rdi, 1             ; stdout
+    lea rsi, [rdi +
