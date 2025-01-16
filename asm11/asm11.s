13c0f9f8fc5436c971fd71dbdac88a19d5cdb5a3
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-16 12:51:38 +0100

    Update asm11.s

--- asm11/asm11.s
+++ asm11/asm11.s
@@ -1,74 +1,124 @@
-section .data
-    vowels db "aeiouAEIOU", 0
-    newline db 10
+global _start
 
 section .bss
     input resb 256
-    output resb 16
+    output resb 12
+
+section .data
+    help: db "Return the number of vowels in the word passed as input", 10
+    .lenHelp: equ $ - help
 
 section .text
-    global _start
-
 _start:
     mov rax, 0
     mov rdi, 0
-    lea rsi, [input]
+    mov rsi, input
     mov rdx, 256
     syscall
 
-    xor rbx, rbx
+    cmp rax, 0
+    jle _exit
 
-    lea rsi, [input]
-    mov rcx, rax
+    xor rdi, rdi
+    xor r8, r8
 
-count_vowels:
-    lodsb
-    test al, al
-    jz convert_to_string
+._code:
+    mov al, [input + rdi]
+    inc rdi
+    cmp al, 10
+    je ._end
 
-    lea rdi, [vowels]
-    mov rdx, 10
-check_vowel:
-    scasb
-    je increment_count
-    dec rdx
-    jnz check_vowel
+    cmp al, 'a'
+    je ._found
+    cmp al, 'e'
+    je ._found
+    cmp al, 'i'
+    je ._found
+    cmp al, 'o'
+    je ._found
+    cmp al, 'u'
+    je ._found
+    cmp al, 'y'
+    je ._found
+    cmp al, 'A'
+    je ._found
+    cmp al, 'E'
+    je ._found
+    cmp al, 'I'
+    je ._found
+    cmp al, 'O'
+    je ._found
+    cmp al, 'U'
+    je ._found
+    cmp al, 'Y'
+    je ._found
+    jmp ._code
 
-    jmp count_vowels
+._found:
+    inc r8
+    jmp ._code
 
-increment_count:
-    inc rbx
-    jmp count_vowels
+._end:
+    mov rax, r8
+    mov rsi, output
+    call std__to_string
 
-convert_to_string:
-    lea rdi, [output + 15]
-    mov byte [rdi], 0
-    mov rax, rbx
+    mov byte [rsi + rdx], 10
+    inc rdx
 
-convert_loop:
-    dec rdi
-    xor rdx, rdx
-    mov rcx, 10
-    div rcx
-    add dl, '0'
-    mov [rdi], dl
-    test rax, rax
-    jnz convert_loop
-
-print_result:
     mov rax, 1
     mov rdi, 1
-    mov rsi, rdi
-    lea rdx, [output + 16]
-    sub rdx, rdi
+    mov rsi, output
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [newline]
-    mov rdx, 1
-    syscall
-
+_exit:
     mov rax, 60
     xor rdi, rdi
     syscall
+
+std__to_string:
+    push rsi
+    push rax
+
+    mov rdi, 1
+    mov rcx, 1
+    mov rbx, 10
+
+.get_divisor:
+    xor rdx, rdx
+    div rbx
+
+    cmp rax, 0
+    je ._after
+    imul rcx, 10
+    inc rdi
+    jmp .get_divisor
+
+._after:
+    pop rax
+    push rdi
+
+.to_string:
+    xor rdx, rdx
+    div rcx
+
+    add al, '0'
+    mov [rsi], al
+    inc rsi
+
+    push rdx
+    xor rdx, rdx
+    mov rax, rcx
+    mov rbx, 10
+    div rbx
+    mov rcx, rax
+
+    pop rax
+
+    cmp rcx, 0
+    jg .to_string
+
+    mov byte [rsi + rdx], 0
+    pop rdx
+    pop rsi
+    ret
