6db0462e815c90c44c230480293bf7f492c8f046
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-17 15:14:32 +0100

    Update asm16.s

--- asm16/asm16.s
+++ asm16/asm16.s
@@ -2,42 +2,63 @@ section .text
 global _start
 
 _start:
+    ; Vérifier le nombre d'arguments
+    mov rax, 1                  
+    mov rdi, 0                   
+    cmp rdi, 2                 
+    je continue                  
+    jmp no_param                 
+
+continue:
     ; Ouvrir le fichier
-    mov rax, 2                   ; Appel système open
-    lea rdi, [rel filename]      ; Nom du fichier
-    mov rsi, 2                   ; O_RDWR
-    mov rdx, 0                   ; Pas de mode nécessaire
+    mov rax, 2                 
+    lea rdi, [rel filename]      
+    mov rsi, 2                 
+    mov rdx, 0                   
     syscall
     test rax, rax
-    js fail_open                 ; Si erreur, aller à fail_open
-    mov r12, rax                 ; Stocker le descripteur de fichier (fd)
+    js fail_open                 
+    mov r12, rax                 
 
     ; Déplacer le curseur à l'offset 0x2000
-    mov rax, 8                   ; Appel système lseek
-    mov rdi, r12                 ; Descripteur de fichier
-    mov rsi, 0x2000              ; Offset
-    mov rdx, 0                   ; SEEK_SET
+    mov rax, 8                   
+    mov rdi, r12                 
+    mov rsi, 0x2000              
+    mov rdx, 0                  
     syscall
     test rax, rax
-    js fail_lseek                ; Si erreur, aller à fail_lseek
+    js fail_lseek               
 
     ; Écrire "H4CK"
-    mov rax, 1                   ; Appel système write
-    mov rdi, r12                 ; Descripteur de fichier
-    lea rsi, [rel new_message]   ; Nouveau message
-    mov rdx, 5                   ; Taille du message (4 caractères + saut de ligne)
+    mov rax, 1                  
+    mov rdi, r12                 
+    lea rsi, [rel new_message]   
+    mov rdx, 5                   
     syscall
     test rax, rax
-    js fail_write                ; Si erreur, aller à fail_write
+    js fail_write                
 
     ; Fermer le fichier
-    mov rax, 3                   ; Appel système close
-    mov rdi, r12                 ; Descripteur de fichier
+    mov rax, 3                   
+    mov rdi, r12                 
     syscall
 
     ; Sortie réussie
-    mov rax, 60                  ; Appel système exit
-    xor rdi, rdi                 ; Code de sortie 0
+    mov rax, 60                  
+    xor rdi, rdi                 
+    syscall
+
+no_param:
+    ; Afficher un message d'erreur
+    mov rax, 1                  
+    mov rdi, 1                  
+    lea rsi, [rel error_message] 
+    mov rdx, error_len           
+    syscall
+
+    ; Quitter avec code de sortie 1
+    mov rax, 60                 
+    mov rdi, 1                   
     syscall
 
 fail_open:
@@ -75,6 +96,8 @@ exit:
 
 section .data
 filename db 'asm01', 0
+error_message db 'Erreur: Aucun parametre fourni.', 0xA, 0
+error_len equ $ - error_message
 open_error db 'Erreur: impossible d ouvrir le fichier.', 0xA, 0
 open_error_len equ $ - open_error
 lseek_error db 'Erreur: impossible de deplacer le curseur.', 0xA, 0
