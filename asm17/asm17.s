643d2cbe9d386ddbec8240f0e544dcf643d3d30e
Adam Lebardan <131999689+adamlbn@users.noreply.github.com>
2025-01-17 16:09:14 +0100

    Create asm17.s

---
+++ asm17/asm17.s
@@ -0,0 +1,99 @@
+section .data
+    SYS_READ  equ 0
+    SYS_WRITE equ 1
+    SYS_EXIT  equ 60
+
+    usage db 'Usage: ./asm17 <shift>', 0xA
+    usage_len equ $ - usage
+
+section .bss
+    input resb 256
+    output resb 256
+
+section .text
+global _start
+
+_start:
+    mov rcx, [rsp]
+    cmp rcx, 2
+    jl .error
+
+    mov rsi, [rsp + 16]
+    call atoi
+    mov r12, rax
+
+    mov rax, SYS_READ
+    mov rdi, 0
+    mov rsi, input
+    mov rdx, 256
+    syscall
+
+    lea rsi, [input]
+    lea rdi, [output]
+    mov rcx, rax
+
+.caesar_loop:
+    lodsb
+    test al, al
+    jz .done
+
+    cmp al, 'a'
+    jb .store_char
+    cmp al, 'z'
+    ja .store_char
+
+    add al, r12b
+    cmp al, 'z'
+    jbe .store_char
+    sub al, 26
+
+.store_char:
+    stosb
+    loop .caesar_loop
+
+.done:
+    sub rdi, output
+    mov rdx, rdi
+
+    mov rax, SYS_WRITE
+    mov rdi, 1
+    mov rsi, output
+    syscall
+
+    mov rax, SYS_EXIT
+    xor rdi, rdi
+    syscall
+
+.error:
+    mov rax, SYS_WRITE
+    mov rdi, 1
+    mov rsi, usage
+    mov rdx, usage_len
+    syscall
+
+    mov rax, SYS_EXIT
+    mov rdi, 1
+    syscall
+
+atoi:
+    xor rax, rax
+    xor rcx, rcx
+
+.convert_loop:
+    movzx rdx, byte [rsi + rcx]
+    test rdx, rdx
+    jz .done_convert
+
+    cmp rdx, '0'
+    jb .done_convert
+    cmp rdx, '9'
+    ja .done_convert
+
+    sub rdx, '0'
+    imul rax, rax, 10
+    add rax, rdx
+    inc rcx
+    jmp .convert_loop
+
+.done_convert:
+    ret
